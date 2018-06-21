d29412292c449268d0812b6109d6da2a94f3469e
leannsmyth <leannmsmyth@gmail.com>
2018-06-21 20:56:17 +0000

    automatically backed up by learn

--- gnomes.rb
+++ gnomes.rb
@@ -1,9 +1,11 @@
 class GardenGnome
   
-  attr_accessor :gluten_allergy :hat_color
+  attr_accessor :gluten_allergy, :hat_color
   attr_reader :personality
-  attr_writer :gnaw :shout :introduce_self
+  attr_writer :gnaw, :shout, :introduce_self
   def initialize(gluten_allergy, personality, hat_color = "red", gnaw, shout, introduce_self)
+    @name = name
+    @age = age
     @gluten_allergy = gluten_allergy
     @personality = personality
     @hat_color = hat_color
@@ -15,3 +17,5 @@ class GardenGnome
 end
 
 gnome1 = GardenGnome.new("Walter the Worst")
+gnome2 = GardenGnome.new("James the Jerk", "3421")
+gnome3 = GardenGnome.new("Alfred the Abhorrent", "579", "true")
