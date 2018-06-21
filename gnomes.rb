fa18486b5cc75d51d961166d1dabe719fbde2cfd
leannsmyth <leannmsmyth@gmail.com>
2018-06-21 20:45:05 +0000

    automatically backed up by learn

--- gnomes.rb
+++ gnomes.rb
@@ -1,13 +1,15 @@
 class GardenGnome
   
-  attr_accessor :gluten_allergy :personality :hat_color :gnaw :shout :intoduce_self
-  attr_accessor :
-  def initialize(gluten_allergy, personality, hat_color, gnaw, shout, introduce_self)
+  attr_accessor :gluten_allergy :hat_color
+  attr_reader :personality
+  attr_writer :graw :shout :introduce_self
+  def initialize(gluten_allergy, personality, hat_color = "red", gnaw, shout, introduce_self)
     @gluten_allergy = gluten_allergy
     @personality = personality
     @hat_color = hat_color
-    @gnaw = gnaw
-    @shout = shout
-    @introduce_self = introduce_self
+    @gnaw = "Gnawing on a tree!!!"
+    @shout = "GNARLY"
+    @introduce_self = "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+    
   end
 end
