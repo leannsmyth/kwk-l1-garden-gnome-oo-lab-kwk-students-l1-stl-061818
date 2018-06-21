1f7b559e3aa37f7edd3033309c49b2af82ea35f2
leannsmyth <leannmsmyth@gmail.com>
2018-06-21 20:57:18 +0000

    automatically backed up by learn

--- gnomes.rb
+++ gnomes.rb
@@ -19,3 +19,5 @@ end
 gnome1 = GardenGnome.new("Walter the Worst")
 gnome2 = GardenGnome.new("James the Jerk", "3421")
 gnome3 = GardenGnome.new("Alfred the Abhorrent", "579", "true")
+
+puts gnome3.gnaw
