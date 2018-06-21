45816fd927c18e9a4b2b9f0042df7497ebefcb9e
leannsmyth <leannmsmyth@gmail.com>
2018-06-21 20:34:15 +0000

    automatically backed up by learn

--- gnomes.rb
+++ gnomes.rb
@@ -1,6 +1,7 @@
 class GardenGnome
   
   attr_accessor :gluten_allergy :personality :hat_color :gnaw :shout :intoduce_self
+  attr_accessor :
   def initialize(gluten_allergy, personality, hat_color, gnaw, shout, introduce_self)
     @gluten_allergy = gluten_allergy
     @personality = personality
